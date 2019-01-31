b581fe334eeb764fdfb58091c7371101f1a6ce6a
Sai Akhil <sai.akhil@tellius.com>
2019-01-31 14:53:17 +0530

    added before in line

--- first.scala
+++ first.scala
@@ -1,4 +1,4 @@
-finallyy resolving merge conflicts 123
+checking here finallyy resolving merge conflicts 123
 
 
 just adding new line and merging into master from test_branch3 - adding in same line
